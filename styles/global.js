7d5cc02517e781f4dc8e8b3ae5f51f913b547f54
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2020-11-14 00:06:20 +0300

    bütün proje için kullanılacak style kodları

---
+++ styles/global.js
@@ -0,0 +1,43 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles= StyleSheet.create({
+    container: {
+        padding: 24,
+        
+    },
+    mainTitleText: {
+        fontFamily: 'nunito-bold',
+        fontSize: 28,
+        color: 'ghostwhite',
+        padding:20,
+        backgroundColor: "black",
+        
+        
+        
+    },
+    titleText:{
+        fontFamily: 'nunito-bold',
+        fontSize: 28,
+        color: 'ghostwhite',
+        padding:20,
+        backgroundColor: "grey",
+    },
+    paragraph:{
+
+    },
+    normalText:{
+        fontFamily: 'nunito-bold',
+        fontSize: 18,
+        marginHorizontal: 153,
+    },
+    image:{
+        height:150,
+        width:150,
+        resizeMode: 'center',
+        marginHorizontal:110,
+        marginVertical:30,
+        borderRadius:30,
+        
+
+    }
+})
