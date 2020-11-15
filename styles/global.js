beffbf793ede902f01c61d3ee962e65164ae5883
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2020-11-16 00:46:15 +0300

    update styles

--- styles/global.js
+++ styles/global.js
@@ -2,42 +2,100 @@ import { StyleSheet } from "react-native";
 
 export const globalStyles= StyleSheet.create({
     container: {
-        padding: 24,
+    
         
     },
-    mainTitleText: {
+    mainHeader: {
         fontFamily: 'nunito-bold',
         fontSize: 28,
         color: 'ghostwhite',
         padding:20,
         backgroundColor: "black",
+        width: '100%',
+        justifyContent: 'center', 
+        alignItems: 'center',
         
         
         
     },
-    titleText:{
+    header:{
         fontFamily: 'nunito-bold',
         fontSize: 28,
         color: 'ghostwhite',
         padding:20,
         backgroundColor: "grey",
+        
+        justifyContent: 'center',
+        alignItems: 'center',
+        
     },
-    paragraph:{
+    title:{
+        fontFamily: 'nunito-bold',
+        fontSize: 20,
+        marginHorizontal: 40
+
 
     },
-    normalText:{
+    optionText:{
         fontFamily: 'nunito-bold',
-        fontSize: 18,
-        marginHorizontal: 153,
+        fontSize: 22,
+        marginHorizontal: '41%',
     },
     image:{
         height:150,
         width:150,
-        resizeMode: 'center',
-        marginHorizontal:110,
+        resizeMode: 'cover',
+        marginHorizontal:'32%',
+        marginVertical:20,
+        borderRadius:20,
+        
+    },
+    image2:{
+        height:150,
+        width:150,
+        resizeMode: 'cover',
         marginVertical:30,
         borderRadius:30,
+        marginHorizontal:30,
+    },
+    sozlesme:{
+        fontFamily: 'nunito-bold',
+        marginHorizontal:5,
+        fontSize:14,
+        color:'white',
+        marginLeft:12,
+        paddingBottom:20,
+        justifyContent: 'center',
+        alignItems: 'center',
         
+    },
+    icons:{
+        marginHorizontal:2,
+        padding:3,
+        alignItems:'center',
+        marginLeft:"38%"
+    },
+    down:{
+        backgroundColor:'black',
+        flexDirection:'row',
+        flexWrap:'wrap',
+        marginTop:'2%'
+        
+    },
+    filter:{
 
+    },
+    searchBar:{
+
+    },
+    itemText:{
+        
+    },
+    cloud:{
+        
+        justifyContent: 'center',
+        alignItems: 'center',
+        
     }
+
 })
