debfb661f0f92657dacf9b00c8ece58a3e60e1a0
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2020-11-18 23:44:18 +0300

    code formatted

--- navigation/homestack.js
+++ navigation/homestack.js
@@ -1,24 +1,23 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Home from '../screens/home';
-import Movies from '../screens/movies';
-import Series from '../screens/series';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "../screens/home";
+import Movies from "../screens/movies";
+import Series from "../screens/series";
 
-const {Navigator, Screen} = createStackNavigator();
-const BulutStack =()=>{
-    return (
-        <Navigator headerMode="None">
-            <Screen name="Home" component={Home} />
-            <Screen name="Movies" component={Movies} />
-            <Screen name="Series" component={Series} />
-        </Navigator>
-     );
-}
+const { Navigator, Screen } = createStackNavigator();
+const BulutStack = () => {
+  return (
+    <Navigator headerMode="None">
+      <Screen name="Home" component={Home} />
+      <Screen name="Movies" component={Movies} />
+      <Screen name="Series" component={Series} />
+    </Navigator>
+  );
+};
 
-export const AppNavigator =()=>(
-    <NavigationContainer>
-        <BulutStack />
-    </NavigationContainer>
+export const AppNavigator = () => (
+  <NavigationContainer>
+    <BulutStack />
+  </NavigationContainer>
 );
